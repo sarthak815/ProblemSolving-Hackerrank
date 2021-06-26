0bb5b951b3f3368af566f6c1fae917731a747d6d
Sarthak Shastri <55555570+sarthak815@users.noreply.github.com>
2021-06-26 23:45:28 +0530

    Day6(1)

---
+++ Binary_Search_Tree_Insertion.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  	
+  	void preOrder(Node *root) {
+		
+      	if( root == NULL )
+          	return;
+      
+      	std::cout << root->data << " ";
+      	
+      	preOrder(root->left);
+      	preOrder(root->right);
+    }
+
+/*
+Node is defined as 
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+*/
+
+    Node * insert(Node * root, int data) {
+            if(root==NULL)
+                return new Node(data);
+            Node* curr=root;
+            Node* temp;
+            while(curr!=NULL){
+                temp=curr;
+                if(curr->data<data)
+                    curr=curr->right;
+                else
+                    curr=curr->left;
+            }
+            if(temp->data<data)
+                temp->right=new Node(data);
+            
+            else
+                temp->left=new Node(data);
+            
+            return root;
+    }
+
+};
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  	
+    myTree.preOrder(root);
+  
+    return 0;
+}
