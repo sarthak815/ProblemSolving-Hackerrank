dfc881efe644de927137337edb61ab428c1202b6
Sarthak Shastri <55555570+sarthak815@users.noreply.github.com>
2021-07-01 22:20:35 +0530

    Day9

---
+++ Algorithm-Problems/Migratory_Birds.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+int migratoryBirds(vector<int> arr) {
+    int mode = 0;
+    int mode1 = 0;
+    int ans = arr.front();
+    set<int> track;
+    for(auto it = arr.begin();it!=arr.end();it++){
+        if(track.find(*it)==track.end()||track.empty()){
+            track.insert(*it);
+        for(auto it1=arr.begin();it1!=arr.end();it1++){
+            if(*it==*it1){
+                mode1++;
+            }
+        }
+        if(mode1>mode){
+            mode = mode1;
+            mode1 = 0;
+            ans = *it;
+        }
+        if(mode1==mode){
+            ans = min(ans,*it);
+        }
+        
+        }
+        else
+            continue;
+    }
+    return ans;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string arr_count_temp;
+    getline(cin, arr_count_temp);
+
+    int arr_count = stoi(ltrim(rtrim(arr_count_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(arr_count);
+
+    for (int i = 0; i < arr_count; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = migratoryBirds(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
