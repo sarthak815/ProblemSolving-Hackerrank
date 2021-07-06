23b5cd364f4a678ddfe203854971b20ac4fc5800
Sarthak Shastri <55555570+sarthak815@users.noreply.github.com>
2021-07-06 21:52:09 +0530

    Day10

---
+++ Algorithm-Problems/Designer_PDF_Viewer.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+int GetPosition(char c)
+{
+   if (c >= 'a' && c <= 'z') {
+      return c - 'a';
+   }
+   else if (c >= 'A' && c <= 'Z') {
+      return c - 'A';
+   }
+   else  {
+      // Indicate that it isn't a letter.
+      return -1;
+   }
+}
+
+int designerPdfViewer(vector<int> h, string word) {
+    int max = 0;
+    for(int i=0;i<word.length();i++){
+        int h1 = h[GetPosition(word[i])];
+        if(h1>max)
+            max = h1; 
+    }
+    return word.length()*max;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string h_temp_temp;
+    getline(cin, h_temp_temp);
+
+    vector<string> h_temp = split(rtrim(h_temp_temp));
+
+    vector<int> h(26);
+
+    for (int i = 0; i < 26; i++) {
+        int h_item = stoi(h_temp[i]);
+
+        h[i] = h_item;
+    }
+
+    string word;
+    getline(cin, word);
+
+    int result = designerPdfViewer(h, word);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
---
+++ Algorithm-Problems/Picking_Numbers.cpp
@@ -0,0 +1,113 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'pickingNumbers' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+int pickingNumbers(vector<int> a) {
+    sort(a.begin(),a.end());
+    map<int,int> mp;
+    int res = 0;
+    for(auto it=a.begin();it!=a.end();it++){
+        mp.insert({*it,count(a.begin(),a.end(),*it)});
+    }
+    int p =mp.begin()->first;
+    int k =mp.begin()->second;
+    for(auto it=mp.begin();it!=mp.end();it++){
+        int n = it->first-p;
+        if(n==1){
+            k = it->second+k;
+            if(k>res)
+                res=k;
+        }
+        if(n==0||n>1){
+            if(k>res)
+                res=k;
+        }
+        
+        p = it->first;
+        k = it->second;
+    }
+    
+    return res;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string a_temp_temp;
+    getline(cin, a_temp_temp);
+
+    vector<string> a_temp = split(rtrim(a_temp_temp));
+
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++) {
+        int a_item = stoi(a_temp[i]);
+
+        a[i] = a_item;
+    }
+
+    int result = pickingNumbers(a);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
